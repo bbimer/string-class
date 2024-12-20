b0ed347469f99b656c95feb55ddeb400d9239146
User <asphaltlev@gmail.com>
2024-12-21 00:24:01 +0200

    added assignment overload operator "=", and string addition "+"

--- string.cpp
+++ string.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class String
@@ -53,6 +54,60 @@ public:
 		}
 	}
 
+	String& operator=(const String& other) {
+		if (this == &other) return *this;
+
+		delete[] newArr;
+		newArr = nullptr;
+
+		size = other.size;
+		if (other.newArr != nullptr)
+		{
+			newArr = new char[size + 1];
+			for (size_t i = 0; i < size; ++i)
+			{
+				newArr[i] = other.newArr[i];
+			}
+			newArr[size] = '\0';
+		}
+		else
+		{
+			for (size_t i = 0; i < sizeof(wrd); ++i)
+			{
+				wrd[i] = other.wrd[i];
+			}
+		}
+		return *this;
+	}
+
+	String operator+(const String& other) const {
+		int new_size = size + other.size;
+		char* new_array = new char[new_size + 1];
+		new_array[0] = '\0';
+
+		if (newArr != nullptr)
+		{
+			strcat_s(new_array, new_size + 1, newArr);
+
+		}
+		else
+		{
+			strcat_s(new_array, new_size + 1, wrd);
+		}
+
+		if (other.newArr != nullptr)
+		{
+			strcat_s(new_array, new_size + 1, other.newArr);
+		}
+		else
+		{
+			strcat_s(new_array, new_size + 1, other.wrd);
+		}
+		String result(new_array);
+		delete[] new_array;
+		return result;
+	}
+
 	~String() {
 		delete[] newArr;
 	}
@@ -79,7 +134,7 @@ int main() {
 	str2 = str1;
 	str2.print();
 
-	String str3("here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.");
+	String str3 = str1 + str2;
 	str3.print();
 
 	return 0;
